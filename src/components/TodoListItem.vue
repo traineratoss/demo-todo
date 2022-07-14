dc94c5726689eb8aee1937611cc3c8d6cfaf4f90
damianlaur7 <108667388+damianlaur7@users.noreply.github.com>
2022-07-14 15:29:50 +0300

    Update TodoListItem.vue

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -7,10 +7,33 @@ defineProps({
 </script>
 
 <template>
-    <input type="checkbox" :checked="completed" />
-    <p>{{ title }}</p>
-    <button>Delete</button>
+    <div class="grid-container">
+        <input type="checkbox" :checked="completed" />
+        <p>{{ title }}</p>
+        <button>Delete</button>
+    </div>
 </template>
 
 <style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  background-color: #2196F3;
+  padding: 10px;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  font-size: 30px;
+  text-align: center;
+}
+button{
+    justify-self: stretch;
+}
+input{
+    justify-self: stretch;
+}
+p{
+    justify-self: stretch;
+}
 </style>
