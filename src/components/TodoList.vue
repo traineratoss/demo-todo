2225b6884ad97328bc4ef55ccede8ea8421358dd
traineratoss2 <traineratoss2@gmail.com>
2022-07-15 11:50:24 +0300

    Add reactivity and event listeners

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,23 +1,61 @@
 <script setup>
 import TodoListItem from './TodoListItem.vue';
+import { ref, reactive, toRefs, watch } from 'vue';
 
 const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-const todos = await response.json();
-const nameVar = "def";
+let todos = await response.json();
+todos = todos.slice(0, 10);
+const reactiveTodos = ref(todos);
+
+const counter = reactive({
+    deleted: 0,
+    updated: 0
+});
+// const counter = ref(0);
+
+watch(() => counter.updated, (newValue, oldValue) => {
+    console.log(`Counter updated ${oldValue} -> ${newValue}`);
+});
+
+function handleTodoItemDeleted(todoItemId) {
+    reactiveTodos.value = reactiveTodos.value.filter(item => item.id !== todoItemId);
+    counter.deleted++;
+}
+
+function handleTodoItemCompleted(todoItemId, completed) {
+    reactiveTodos.value = reactiveTodos.value.map(todo => {
+        if (todo.id === todoItemId) {
+            return {
+                ...todo,
+                completed: completed
+            }
+        } else {
+            return todo;
+        }
+    });
+    counter.updated++;
+}
 </script>
 
 <template>
-    <ul>
+    <p>{{ counter.updated }}</p>
+    <p>{{ counter.deleted }}</p>
+    <div class="todo-list">
         <TodoListItem
-            v-for="todo of todos"
+            v-for="todo of reactiveTodos"
             :key="todo.id"
             :id="todo.id"
             :title="todo.title"
             :completed="todo.completed"
+            @todo-item-deleted="handleTodoItemDeleted(todo.id)"
+            @todo-item-completed="completed => handleTodoItemCompleted(todo.id, completed)"
         />
-    </ul>
+    </div>
 </template>
 
 <style scoped>
-
+.todo-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
 </style>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,15 +1,25 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
     id: Number,
     title: String,
     completed: Boolean
-})
+});
+
+const emit = defineEmits(['todoItemDeleted', 'todoItemCompleted']);
+
+const newTitle = computed(() => `${props.id}. ${props.title}[${props.completed}]`);
+
+function handleChange(event) {
+    emit('todoItemCompleted', event.target.checked);
+}
 </script>
 
 <template>
-    <input type="checkbox" :checked="completed" />
-    <p>{{ title }}</p>
-    <button>Delete</button>
+    <input type="checkbox" :checked="completed" @change="handleChange" />
+    <p>{{ newTitle }}</p>
+    <button @click="$emit('todoItemDeleted')">Delete</button>
 </template>
 
 <style scoped>
